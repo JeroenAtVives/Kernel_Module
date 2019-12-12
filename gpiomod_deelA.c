169289f72eaf4c9b86ceed988027518cdad4b43b
Jeroen Marly <r0710937@student.vives.be>
2019-12-12 08:31:20 +0000

    deelA_1

--- gpiomod_deelA.c
+++ gpiomod_deelA.c
@@ -22,12 +22,62 @@
 #include <linux/init.h>
 #include <linux/gpio.h>
 
-#define PIN16	16
-#define PIN18	18
+// Variabelen die de pinnummers bijhouden...
+int pinX = 16;
+int pinY = 18;
 
 static struct timer_list blink_timer;
 static long data=0;
 
+/*
+ * The module commandline arguments ...
+ */
+static int 			 ioToggleSpeed	= 1;
+static int 			 ioNummers[2] 	= { -1, -1 };
+static int 			 arr_argc 		= 0;
+
+module_param(ioToggleSpeed, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(ioToggleSpeed, "An integer that describes how fast the IO toggles");
+module_param_array(ioNummers, int, &arr_argc, 0000);
+MODULE_PARM_DESC(ioNummers, "An array of integers met de IO-nummers");
+
+/*
+ * Clargmod init function
+ */
+static int clargmod_init(void)
+{
+	int i;
+
+	printk(KERN_INFO "%s\n=============\n", __func__); // __func__ is de naam van de "enclosing function", hier clargmod_init.
+	printk(KERN_INFO "ioToggleSpeed is an integer: %d\n", ioToggleSpeed);
+
+	// Inhoud van de array met ioNummers tonen...
+	for (i = 0; i < (sizeof ioNummers / sizeof (int)); i++)
+	{
+		printk(KERN_INFO "ioNummers[%d] = %d\n", i, ioNummers[i]);
+	}
+
+	// Controle op de inhoud van de array met ioNummers. Op basis van die inhoud worden de pinnen pinX en pinY juist gezet. 
+	// Want het kan zijn dat de array leeg was, dat er 1 nummer in zat of 2 nummers in zaten.
+
+	// Zit er niets op index 0 dan zal er ook niets zitten op index 1 dus krijgen pinX en pinY default waarden.
+	if(ioNummers[0] == -1){
+		pinX = 16;
+	}else{
+		pinX = ioNummers[0];
+	}
+	if(ioNummers[1] == -1){
+		pinY = 18;
+	}else{
+		pinY = ioNummers[1];
+	}
+	printk(KERN_INFO "pinX : %d\n",pinX);
+	printk(KERN_INFO "pinY : %d\n",pinY);	
+	// Hoeveel argumenten zaten er in de array ioNummers...
+	printk(KERN_INFO "got %d arguments for ioNummers.\n", arr_argc);
+
+	return 0;
+}
 /*
  * Timer function called periodically
  */
@@ -35,13 +85,12 @@ static void blink_timer_func(struct timer_list* t)
 {
 	printk(KERN_INFO "%s\n", __func__);
 
-	gpio_set_value(PIN16, data);
-	gpio_set_value(PIN18, data);
+	gpio_set_value(pinX, data);
+	gpio_set_value(pinY, data);
 	data=!data; 
 	
 	/* schedule next execution */
-	//blink_timer.data = !data;						// makes the LED toggle 
-	blink_timer.expires = jiffies + (1*HZ); 		// 1 sec.
+	blink_timer.expires = jiffies + (ioToggleSpeed*HZ); 		
 	add_timer(&blink_timer);
 }
 
@@ -50,14 +99,18 @@ static void blink_timer_func(struct timer_list* t)
  */
 static int __init gpiomod_init(void)
 {
+	// Functie oproepen die zich bezig houdt met het verwerken van de parameters...
+	clargmod_init();
+
+
 	int ret1 = 0;
 	int ret2 = 0;
 
 	printk(KERN_INFO "%s\n", __func__);
 
 	// register, turn off 
-	ret1 = gpio_request_one(PIN16, GPIOF_OUT_INIT_LOW, "pin16");
-	ret2 = gpio_request_one(PIN18, GPIOF_OUT_INIT_LOW, "pin18");
+	ret1 = gpio_request_one(pinX, GPIOF_OUT_INIT_LOW, "pinX");
+	ret2 = gpio_request_one(pinY, GPIOF_OUT_INIT_LOW, "pinY");
 
 
 	if (ret1) {
@@ -74,9 +127,9 @@ static int __init gpiomod_init(void)
 	//init_timer(&blink_timer);
 	 timer_setup(&blink_timer, blink_timer_func, 0);
 
+	// makes the LED toggle for "ioToggleSpeed" sec
 	blink_timer.function = blink_timer_func;
-	//blink_timer.data = 1L;							// initially turn LED on
-	blink_timer.expires = jiffies + (1*HZ); 		// 1 sec.
+	blink_timer.expires = jiffies + (ioToggleSpeed*HZ); 		 
 	add_timer(&blink_timer);
 
 	return ret1;
@@ -92,13 +145,13 @@ static void __exit gpiomod_exit(void)
 	// deactivate timer if running
 	del_timer_sync(&blink_timer);
 
-	// turn LED off
-	gpio_set_value(PIN16, 0); 
-	gpio_set_value(PIN18, 0); 	
+	// Zet de IOpinnen terug laag...
+	gpio_set_value(pinX, 0); 
+	gpio_set_value(pinY, 0); 	
 	
 	// unregister GPIO 
-	gpio_free(PIN16);
-	gpio_free(PIN18);
+	gpio_free(pinX);
+	gpio_free(pinY);
 
 }
 
